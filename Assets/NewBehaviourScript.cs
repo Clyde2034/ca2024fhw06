53ed09ca54a6fa076bdf811957e6e957b06f95ba
DESKTOP-TKIRTNK\Clyde <asd66998854@gmail.com>
2024-12-13 15:39:11 +0800

    changed fbx

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,11 +5,48 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public GameObject videoPlayer;
+	private Animator _animator;
 
     public void VideoStart()
     {
         videoPlayer.SetActive(true);
     }
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        this._animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        //characterMotion(); //鍵盤控制
+    }
+
+    void characterMotion()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _animator.SetTrigger("walkingTrigger");
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            _animator.SetTrigger("runningTrigger");
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            _animator.SetTrigger("idleTrigger");
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            _animator.SetTrigger("motionTrigger");
+        }
+    }
+
 }
 
 
